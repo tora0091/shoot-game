6dd8f5d61560879e62d2a8df2197284204fee336
tora0091 <tora0091@yahoo.co.jp>
2023-11-04 16:58:59 +0900

    update enemy pattern

--- src/enemy/enemy_pattern_001.rs
+++ src/enemy/enemy_pattern_001.rs
@@ -1,6 +1,7 @@
 use std::f32::consts::PI;
 
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use rand::Rng;
 
 use crate::define::*;
 
@@ -12,7 +13,6 @@ impl Plugin for EnemyPattern001 {
     fn build(&self, app: &mut App) {
         app.add_systems(Update, (
             enemy_spawn_pattern_001,
-            enemy_move_pattern_001,
         ));
     }
 }
@@ -29,34 +29,37 @@ pub fn enemy_spawn_pattern_001(
     game_timer: Res<GameTimer>,
 ) {
     if EnemySchedule::is_ready(&mut enemy_schedule.enemy_pattern_001, game_timer.seconds) {
-        let x = 0.0;
         let y = window_size_limit.top - 30.0;
+        let x = window_size_limit.right / 3.0;
 
-        // enemy
-        commands.spawn((
-            MaterialMesh2dBundle {
-                mesh: meshes.add(shape::Circle::new(ENEMY_RADIUS).into()).into(),
-                material: materials.add(ColorMaterial::from(Color::GREEN)),
-                transform: Transform::from_xyz(x, y, 9.0),
-                ..default()
-            },
-            Enemy {
-                shoot_interval: Timer::from_seconds(1.0, TimerMode::Repeating),
-            },
-            AutoDespawn,
-            Velocity {x: 0.0, y: -1.0},
-            EnemyMovePattern001,
-        ));
-    }
-}
+        let enemy_positions = [
+            [x, y],
+            [x * 2.0, y],
+            [0.0, y],
+            [-x, y],
+            [-x * 2.0, y],
+        ];
 
-pub fn enemy_move_pattern_001(
-    mut query: Query<&mut Velocity, With<EnemyMovePattern001>>,
-    time: Res<Time>,
-) {
-    let x = PI * (time.elapsed_seconds() % 60.0).sin();
+        let mut rng = rand::thread_rng();
 
-    for mut velocity in query.iter_mut() {
-        velocity.x = x;
+        for position in enemy_positions {
+            let shot_duration = rng.gen_range(1.0..3.0);
+
+            // enemy
+            commands.spawn((
+                MaterialMesh2dBundle {
+                    mesh: meshes.add(shape::Circle::new(ENEMY_RADIUS).into()).into(),
+                    material: materials.add(ColorMaterial::from(Color::GREEN)),
+                    transform: Transform::from_xyz(position[0], position[1], 9.0),
+                    ..default()
+                },
+                Enemy {
+                    shoot_interval: Timer::from_seconds(shot_duration, TimerMode::Repeating),
+                },
+                AutoDespawn,
+                Velocity {x: 0.0, y: -0.5},
+                EnemyMovePattern001,
+            ));
+        }
     }
 }
